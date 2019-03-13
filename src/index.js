df2863c9d36fae3f24b3740d92db4953c11b277c
janglewood <zhl96@mail.ru>
2019-03-13 23:22:45 +0300

    feat: implement optional functions

--- src/index.js
+++ src/index.js
@@ -1,54 +1,71 @@
 module.exports = function solveSudoku(matrix) {
 
-  function searchRow(rowNum) {
-    let existingNum = [];
+  function getPossibleNum(existingNum) {
     let possibleNum = [];
-    for(let i = 0; i < rowNum.length; i++) {
-      rowNum[i] > 0 ? existingNum.push(rowNum[i]) : null;
-      }
     for(let k = 1; k <= 9; k++) {
       existingNum.indexOf(k) === -1 ? possibleNum.push(k) : null;
     }
-    return possibleNum;
+    return possibleNum;  
+  };
+  
+  function searchRow(rowNum) {
+    let existingNum = [];
+    for(let i = 0; i < rowNum.length; i++) {
+      rowNum[i] > 0 ? existingNum.push(rowNum[i]) : null;
+      }
+    return getPossibleNum(existingNum);
   };
 
   function searchColumn(columnNum) {
     let existingNum = [];
     matrix.forEach(item => item[columnNum] > 0 ? existingNum.push(item[columnNum]) : null);
-    return existingNum;
+    return getPossibleNum(existingNum);
   };
 
   function searchSquad(rowNum, columnNum) {
     let squad = [];
-    if(Math.ceil((columnNum + 1) / 3) <= 1 && Math.ceil((rowNum + 1) / 3) <= 1) {
-      for(let i = 0; i < 3; i++) {
+    if(Math.ceil((columnNum + 1) / 3) === 1) {
+      for(let i = (Math.ceil((rowNum + 1) / 3) * 3) - 3; i < Math.ceil((rowNum + 1) / 3) * 3; i++) {
         for(let k = 0; k < 3; k++) {
           squad.push(matrix[i][k]);
         }
       }
     }
-    if(Math.ceil((columnNum + 1) / 3) <= 2 && Math.ceil((rowNum + 1) / 3) <= 2) {
-      for(let i = 3; i < 6; i++) {
+    if(Math.ceil((columnNum + 1) / 3) === 2) {
+      for(let i = (Math.ceil((rowNum + 1) / 3) * 3) - 3; i < Math.ceil((rowNum + 1) / 3) * 3; i++) {
         for(let k = 3; k < 6; k++) {
           squad.push(matrix[i][k]);
         }
       }
     }
-    if(Math.ceil((columnNum + 1) / 3) === 3 && Math.ceil((rowNum + 1) / 3) === 3) {
-      for(let i = 6; i < 9; i++) {
+    if(Math.ceil((columnNum + 1) / 3) === 3) {
+      for(let i = (Math.ceil((rowNum + 1) / 3) * 3) - 3; i < Math.ceil((rowNum + 1) / 3) * 3; i++) {
         for(let k = 6; k < 9; k++) {
           squad.push(matrix[i][k]);
         }
       }
     }
-    return squad;
+    return getPossibleNum();
   };
+
+  function getUnique() {
+    let unique = [];
+    for(let i = 0; i < arguments.length; i++) {
+      arguments[i].forEach(item => unique.indexOf(item) === -1 ? unique.push(item) : null);
+    }
+    return unique;
+  }
 
   for(let vert = 0; vert < matrix.length; vert++) {
     for(let horz = 0; horz < matrix.length; horz++) {
       if(matrix[vert][horz] === 0) {
-
+        let uniqueRow = searchRow(horz);
+        let uniqueColumn = searchColumn(vert);
+        let uniqueSquad = searchSquad(horz, vert);
+        let unique = getUnique(uniqueRow, uniqueColumn, uniqueSquad);
+        matrix[vert][horz] = unique[0];
       }
     }
   }
+  return matrix;
 }
